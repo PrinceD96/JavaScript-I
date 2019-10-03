9b37afb72a7015a258065f95288c95a99c201eb6
Daniel Morales <daniel.a.moralesalt@gmail.com>
2019-10-02 22:40:13 -0400

    Stretch Tasks done

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -1,23 +1,19 @@
 // Take the commented ES5 syntax and convert it to ES6 arrow Syntax
 
-// let myFunction = () => {
-//   console.log('Function was invoked!');
-// };
-// myFunction();
+let myFunction = () => console.log('Function was invoked!');
+myFunction();
 
-// let anotherFunction = param => param;
-// console.log(anotherFunction('Example'));
+let anotherFunction = param => param;
+console.log(anotherFunction('Example'));
 
-// let add = (param1, param2) => param1 + param2;
-// console.log(add(1, 2));
+let add = (param1, param2) => param1 + param2;
+console.log(add(1, 2));
 
-// let subtract = (param1, param2) => param1 - param2;
-// console.log(subtract(1, 2));
+let subtract = (param1, param2) => param1 - param2;
+console.log(subtract(1, 2));
 
-// Stretch
+// Stretch;
 
-// exampleArray = [1,2,3,4];
-// const triple = exampleArray.map(function (num) {
-//   return num * 3;
-// });
-// console.log(triple);
+exampleArray = [1, 2, 3, 4];
+const triple = exampleArray.map(num => num * 3);
+console.log(triple);
